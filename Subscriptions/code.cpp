c0957dfb4a712d6775266a83f91be7d3cb023331
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-16 23:43:40 +0530

    Update code.cpp

--- Subscriptions/code.cpp
+++ Subscriptions/code.cpp
@@ -1,6 +1,26 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	int n,x;
+	cin>>n>>x;
+	int ans=n/6;
+	if(n<=6){
+	    cout<<x<<endl;
+	}
+	else{
+	    if(n%6==0){
+	    cout<<ans*x<<endl;
+	    }
+	    else{
+	        ans++;
+	        cout<<ans*x<<endl;
+	    }
+	}
+	}
+	return 0;
 }
