361006d6420822cf1055f34c9abc40c127839f24
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-08-21 15:51:34 +0530

    Update code.cpp

--- Air Conditioner Temperature/code.cpp
+++ Air Conditioner Temperature/code.cpp
@@ -1,6 +1,19 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	while(n--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(x<=y && z<=y){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
 }
