3a4262f7d2cb4918729e0a3ab84194025389a198
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-10-02 00:18:31 +0530

    Update code.cpp

--- Equalize AB/code.cpp
+++ Equalize AB/code.cpp
@@ -1,6 +1,18 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+    int a,b,x;
+    cin>>a>>b>>x;
+    if((abs(a-b))%(2*x)==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+    }
+
+    return 0;
 }
