21e3c21cb4f783bc116dfb1195032e14b0f0630c
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-10-17 14:44:48 +0530

    Update code.cpp

--- Difficulty rating/code.cpp
+++ Difficulty rating/code.cpp
@@ -1,6 +1,24 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,count=0;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++){
+	        cin>>a[i];
+	    }
+	    for(int i=0;i<n;i++){
+	    if(a[i]>=1000){
+	        count=count+1;
+	    }
+	    }
+	    cout<<count<<endl;
+	    
+	}
+	return 0;
 }
