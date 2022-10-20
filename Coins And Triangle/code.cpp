002069eacc6e663cc5cb3269bfe0e93e3c0fbaa1
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2022-10-20 23:33:39 +0530

    Update code.cpp

--- Coins And Triangle/code.cpp
+++ Coins And Triangle/code.cpp
@@ -1,6 +1,29 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int count = 0;
+        int r = 1;
+        int h = 0;
+        while(count <= n){
+            count += r;
+            r += 1;
+            h++;
+        }
+        if(n < count){
+            cout<<h-1<<endl;
+        }
+        else{
+            cout<<h<<endl;
+        }
+    }
+
+    return 0;
 }
