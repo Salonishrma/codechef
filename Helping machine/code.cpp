7149280a9202bbcf33a2b6a4f3499dae2c225194
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-01-17 23:43:05 +0530

    Update code.cpp

--- Helping machine/code.cpp
+++ Helping machine/code.cpp
@@ -1,6 +1,34 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	   int n,i;
+	    cin>>n;
+	    int count1=0;
+	    int count2=0;
+	    int count3=0;
+	     int size[n];
+	    for( i=0;i<n;i++){
+	        cin>>size[i];
+	    }
+	    for(int i=0;i<n;i++){
+	    if(size[i]==1 ||size[i]==2){
+	        count1++;
+	    }
+	    else if(size[i]==3 ||size[i]==4){
+	        count2++;
+	    }
+	    else if(size[i]==5 ||size[i]==6){
+	        count3++;
+	    }
+	    }
+	    cout<<"TYPE 1 - "<<count1<<endl;
+	    cout<<"TYPE 2 - "<<count2<<endl;
+	    cout<<"TYPE 3 - "<<count3<<endl;
+	}
+	return 0;
 }
